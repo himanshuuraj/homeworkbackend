1dd233dbfb293c21b5a9d38780d6b28d6e7d4bb0
Rishi Anand <rishi.anand@stu.upes.ac.in>
2018-10-11 23:31:39 +0530

    adding student details

--- models/user.js
+++ models/user.js
@@ -2,11 +2,13 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  name: { type: String, required: true, max: 100 },
-  email: { type: String, required: true },
-  dob: { type: Number, required: true },
-  phone: { type: String, required: true },
-  password: { type: String, required: true }
+	name: { type: String, required: true, max: 100 },
+	email: { type: String, required: true },
+	dob: { type: Number, required: false },
+	phone: { type: String, required: false },
+	password: { type: String, required: true },
+	children: {type: Array, required:true },
+	address: {type: String, required:true }
 });
 
 // Export the model
